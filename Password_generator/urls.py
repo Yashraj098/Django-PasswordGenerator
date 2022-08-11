83e441de94429bff05339fbdb763881bd06e289a
Yashraj098 <83530269+Yashraj098@users.noreply.github.com>
2022-08-11 16:21:37 +0530

    Update urls.py

--- Password_generator/urls.py
+++ Password_generator/urls.py
@@ -18,5 +18,4 @@ from generator import views
 urlpatterns = [
    path('',views.home, name="home"),
    path('password/',views.password, name='password'),
-   path('about/',views.about, name='about'),
 ]
